2fb40322f24fbccc93139354046ccd71c236e413
jedano11 <jomaredano2019@gmail.com>
2025-04-08 23:03:49 +0800

    Styling and using user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { UserComponent } from "./shared/components/user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
